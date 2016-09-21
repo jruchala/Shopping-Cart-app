af46ebc469125d5354287f5dd69a7e060f7f6fc6
jruchala <jruchala@gmail.com>
2016-09-21 14:33:27 -0400

    add links to item details from home page

--- ShoppingCart/Models/CodeFirst/Order.cs
+++ ShoppingCart/Models/CodeFirst/Order.cs
@@ -12,6 +12,9 @@ namespace ShoppingApp.Models
         public int Id { get; set; }
 
         [Required]
+        public string Name { get; set; }
+
+        [Required]
         public string Address { get; set; }
 
         [Required]
